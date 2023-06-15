4d2df9a82a92cbcb64d8d2adaf1cd634a77c2734
Bruno Silvério <brunosantarosasilverio@gmail.com>
2023-06-15 17:06:52 -0300

    first commit

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 const key = '30d6876bb0d2dd1db64e4c98ef424ca0'
 
-
 function changingData(data) {
     console.log(data)
-    document.querySelector('.city').innerHTML = "Tempo em " + data.name
-    document.querySelector('.temp').innerHTML = Math.floor(data.main.temp) + "ºC"
-    document.querySelector('.text').innerHTML = data.weather[0].description
-    document.querySelector('.humidity').innerHTML = "Humidity " + data.main.humidity + '%'
-    document.querySelector('.img-weather').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    document.querySelector('.city').innerHTML = "Weather in " + data.name
+    document.querySelector('.temp').innerHTML = Math.floor(data.main.temp) + 'ºC'
+    document.querySelector('.weather-info').innerHTML = data.weather[0].description
+    document.querySelector('.humidity').innerHTML = "Humidity: " + data.main.humidity + '%'
+    document.querySelector('.weather-icon').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
 }
 
-
 async function searchCity(city) {
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
-
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`).then(response => response.json())
     changingData(data)
 }
 
-function clickButton() {
-    const city = document.querySelector('.input-city').value
+function inputValue() {
+    const city = document.querySelector('.city-input').value
 
     searchCity(city)
 }
-
